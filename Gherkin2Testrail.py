25c18985bec03a8c2a99b0eb7cfb6d802434c7d6
Phillip Wheatley <Phillip@Phil-Windows8.(none)>
2014-02-25 17:36:23 +0100

    Added 'But' support

--- Gherkin2Testrail.py
+++ Gherkin2Testrail.py
@@ -16,7 +16,7 @@ class gherkintotestrailxmlCommand(sublime_plugin.TextCommand):
 		s = re.sub(r'# (.*)\n((.*\n)*?)\n\n', r'\t\t<section>\n\t\t\t<name>\1</name>\n\t\t\t<cases>\n\2\n\t\t\t</cases>\n\t\t</section>\n', s)
 		s = re.sub(r'Scenario( Outline)*: (.*) - (.*)\n((.*\n)*?)\n', r'\t\t\t\t<case>\n\t\t\t\t\t<title>\3</title>\n\t\t\t\t\t<custom>\n\t\t\t\t\t\t<bdd_id>\2</bdd_id>\n\4\t\t\t\t\t</custom>\n\t\t\t\t</case>\n', s)
 		s = re.sub(r'(@.*)\n((.*\n)*?.*</bdd_id>)', r'\2\n\t\t\t\t\t\t<bdd_tags>\1</bdd_tags>', s)
-		s = re.sub(r'\n.*(And (.*))', r'&#xA;\1', s)
+		s = re.sub(r'\n.*((?:And|But) (.*))', r'&#xA;\1', s)
 		s = re.sub(r'\n\t*(\|(.*)\|)', r'&#xA;\1', s)
 		s = re.sub(r'\t*Given (.*)', r'\t\t\t\t\t\t<bdd_given>\1</bdd_given>', s)
 		s = re.sub(r'\t*When (.*)', r'\t\t\t\t\t\t<bdd_when>\1</bdd_when>', s)
@@ -84,13 +84,13 @@ class gherkintotestrailimportsuiteCommand(sublime_plugin.TextCommand):
 				currScenarios = self.client.send_get('get_cases/' + str(self.currProject) + '&suite_id=' + str(suite_id) + '&section_id=' + str(section_id))
 				scenario_id = None
 				for x in currScenarios:
-					if x['title'] == j[2]:
+					if x['custom_bdd_id'] == j[1]:
 						scenario_id = x['id']
 
 		 		tags 		= j[0]
-		 		given 		= re.findall(r'\t*Given ((?:.*)(?:\n\t\tAnd .*)*)', j[3])
-		 		when  		= re.findall(r'\t*When ((?:.*)(?:\n\t\tAnd .*)*)', j[3])
-		 		then 		= re.findall(r'\t*Then ((?:.*)(?:\n\t\tAnd .*)*)', j[3])
+		 		given 		= re.findall(r'\t*Given ((?:.*)(?:\n\t\t(?:And|But) .*)*)', j[3])
+		 		when  		= re.findall(r'\t*When ((?:.*)(?:\n\t\t(?:And|But) .*)*)', j[3])
+		 		then 		= re.findall(r'\t*Then ((?:.*)(?:\n\t\t(?:And|But) .*)*)', j[3])
 		 		examples 	= re.findall(r'\t*Examples:\n((?:\t*.*\n)*)', j[3])
 
 		 		scenarios[j[1]] = {'custom_bdd_id': j[1], 'title': j[2]}
